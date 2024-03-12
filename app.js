b3cf33df37ba18235e50bebd5f0b0a3d81e38dfd
Diana Moretti <morettidn@gmail.com>
2024-03-12 16:38:41 +0100

    updating index and styles

---
+++ app.js
@@ -0,0 +1,49 @@
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".menu-btn");
+const dropdown = document.querySelector(".dropdown__container");
+let accordion = document.querySelectorAll(".accordion");
+
+function toggleDropdown() {
+  dropdown.classList.toggle("hide");
+}
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    let panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+hamburger.addEventListener("click", toggleDropdown);
